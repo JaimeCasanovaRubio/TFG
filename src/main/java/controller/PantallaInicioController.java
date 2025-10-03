93817b9376b3cfb26145d06bca2c1124171b054d
jaime <jaime.casrub4@gmail.com>
2025-10-03 13:08:29 +0200

    tercer commmit ajuste de proyecto

--- src/main/java/controller/PantallaInicioController.java
+++ src/main/java/controller/PantallaInicioController.java
@@ -1,4 +1,17 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class PantallaInicioController {
+
+    @FXML
+    private Button btnCerrar;
+
+    @FXML
+    void cerrar(ActionEvent event) {
+        System.exit(0);
+    }
+
 }
